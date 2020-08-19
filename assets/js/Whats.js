8f9088b2a9db9cc46684f51cbf2b2096e3fdcb4d
Wesley Gonçalves <wesleylg01@gmail.com>
2020-08-19 00:57:33 -0300

    Finished

---
+++ assets/js/Whats.js
@@ -0,0 +1,10 @@
+function GetDate(){
+    var name    = document.getElementById("name").value
+    var number  = document.getElementById("phone").value
+    var message = document.getElementById("message").value
+
+    var teste = (name+number+message)
+
+    alert(teste)
+    
+}
